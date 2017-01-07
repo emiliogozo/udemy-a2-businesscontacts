d1f4d1277bb76a79732e42f032f1ad18cae733c9
Emilio Gozo <emilio.gozo@obf.ateneo.edu>
2017-01-07 11:08:41 +0800

    chore: AngularFire2 Firebase setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { AngularFireModule } from 'angularfire2';
 
 import { AppComponent } from './app.component';
+
+// Must export the config
+export const firebaseConfig = {
+  apiKey: 'AIzaSyA2CcbmKSLvXPxjwQkp5mdwH98BtyQyMHM',
+  authDomain: 'businesscontacts-e9f88.firebaseapp.com',
+  databaseURL: 'https://businesscontacts-e9f88.firebaseio.com',
+  storageBucket: 'businesscontacts-e9f88.appspot.com',
+  messagingSenderId: '996379778945'
+};
 
 @NgModule({
   declarations: [
@@ -12,7 +22,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
